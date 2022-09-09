9b815cf694e084dc3ac407fd4eccfd9d7cca3b35
Blaise <blaiseokere2@gmail.com>
2022-09-08 18:49:38 -0700

    hi

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,19 @@
+#!/bin/bash
+gcc -S -masm=intel $CFILE
+
+Task 101
+
 #include <stdio.h>
+#include <unistd.h>
+
 /**
-   * main - prints to the standard error
-   *
-   * Return: one
-   */
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+        write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
 }
